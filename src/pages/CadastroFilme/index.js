9b1d6ef79e9c0e1668eaf2fc29680921288febba
unknown <murilo.aguiar2303@gmail.com>
2024-03-27 21:32:52 -0300

    Cadastro filmes concluido

--- src/pages/CadastroFilme/index.js
+++ src/pages/CadastroFilme/index.js
@@ -1,20 +1,88 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, Button } from 'react-native';
+import { DatabaseConnection } from '../../database/database'
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const db = new DatabaseConnection.getConnection;
+    const [filme, setFilme] = useState('');
+    const [clas, setClas] = useState('');
+    const [categoria, setCate] = useState('');
+    //Todos precisa aqui ? n sei ainda
+    const salvaFilme = () => {
+        // if (filme.trim() === '' || filme === null) {
+        //     Alert.alert('Erro', 'Por favor, insira um texto válido para adicionar o filme');
+        //     return;
+        // }
+        db.transaction(
+            tx => {
+                tx.executeSql(
+                    'INSERT INTO filmes (nome_filme, genero, classificacao, data_cad) VALUES (?, ?, ?, datetime("now")) ',
+                    [filme, categoria, clas],
+                    (_, { rowsAffected }) => {
+                        console.log(rowsAffected);
+                        setFilme('');
+                        setCate('');
+                        setClas('');
+                    },
+                    (_, error) => {
+                        console.error('Erro ao adicionar o filme:', error);
+                        Alert.alert('Erro', 'Ocorreu um erro ao adicionar o filme.');
+                    }
+                );
+            }
+        );
+    }
+
+
+
+
+
+    return (
+        <View style={styles.container}>
+            {/* titulo */}
+            <Text>Cadastre aqui o seu lindo filme (de terror)</Text>
+            {/* ID e Data tem q entrar mas n tem que digitar */}
+
+            <TextInput
+                style={styles.input}
+                value={filme}
+                onChangeText={setFilme}
+                placeholder="Digite um novo Filme"></TextInput>
+
+            <TextInput
+                style={styles.input}
+                value={categoria}
+                onChangeText={setCate}
+                placeholder="Digite sua categoria"></TextInput>
+
+            <TextInput
+                style={styles.input}
+                value={clas}
+                onChangeText={setClas}
+                placeholder="Digite a clascificação"></TextInput>
+
+            <Button title="Adicionar" onPress={salvaFilme} />
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }, input: {
+        width: '70%',
+        borderWidth: 1,
+        borderColor: 'red',
+        borderRadius: 15,
+        padding: 10,
+        marginBottom: 10,
+        textAlign: 'center'
+    },
 });
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
+import React, { useEffect,useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { DatabaseConnection } from '../../database/database'
 import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
+    const db = new DatabaseConnection.getConnection; 
     const navigation = useNavigation();
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        db.transaction(tx => {
+          tx.executeSql(
+            'CREATE TABLE IF NOT EXISTS filmes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome_filme TEXT NOT NULL, genero TEXT NOT NULL, classificacao TEXT NOT NULL, data_cad TEXT NOT NULL)',
+            [], 
+            () => console.log('Tabela criada 👍'),
+            (_, error) => console.error(error) 
+          );
+        });
+      }, [todos]);
+
+
 
     function Cadastro() {
         navigation.navigate('CadastroFilme')
